719479d983d69cc07a6bb64cf58fa5a4613474d0
Alex <kovalovs.alex1@gmail.com>
2020-07-13 12:11:12 +0300

    Initial commit

---
+++ network/src/TCPConnection.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+public class TCPConnection {
+    private final Socket socket;
+    private final Thread rxThread;
+    private final TCPConnectionListener eventListener;
+    private final BufferedReader in;
+    private final BufferedWriter out;
+
+    public TCPConnection(TCPConnectionListener eventListener, String ipAddr, int port) throws IOException {
+        this(new Socket(ipAddr, port), eventListener);
+    }
+
+
+    public TCPConnection(Socket socket, TCPConnectionListener eventListener) throws IOException {
+        this.eventListener = eventListener;
+        this.socket = socket;
+        in = new BufferedReader((new InputStreamReader(socket.getInputStream(), Charset.forName("UTF-8"))));
+        out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), Charset.forName("UTF-8")));
+        rxThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    eventListener.onConnectionReady(TCPConnection.this);
+                    while (!rxThread.isInterrupted()) {
+                        String msg = in.readLine();
+                        eventListener.onReceiveString(TCPConnection.this, msg);
+                    }
+
+                } catch (IOException e) {
+                    eventListener.onException(TCPConnection.this, e);
+                    disconnect();
+                }
+            }
+        });
+        rxThread.start();
+    }
+
+    public synchronized void sendString(String value) {
+        try {
+            out.write(value + "\r\n");
+            out.flush();
+        } catch (IOException e) {
+            eventListener.onException(TCPConnection.this, e);
+            disconnect();
+        }
+
+    }
+
+    public synchronized void disconnect() {
+        rxThread.interrupt();
+        try {
+            socket.close();
+        } catch (IOException e) {
+            eventListener.onException(TCPConnection.this, e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "TCPConnection: " + socket.getInetAddress() + ": " + socket.getPort();
+    }
+
+}
---
+++ server/src/ChatServer.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+
+public class ChatServer implements TCPConnectionListener {
+    public static void main(String[] args) {
+        new ChatServer();
+    }
+
+    private final ArrayList<TCPConnection> connections = new ArrayList<>();
+
+    private ChatServer() {
+        System.out.println("Server running...");
+        try (ServerSocket serverSocket = new ServerSocket(8189)) {
+            while (true) {
+                try {
+                    new TCPConnection(serverSocket.accept(), this);
+                } catch (IOException ex) {
+                    System.out.println("TCPConnection exception : " + ex);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public synchronized void onConnectionReady(TCPConnection tcpConnection) {
+        connections.add(tcpConnection);
+        sendToAllConnections("Client connected: " + tcpConnection);
+    }
+
+    @Override
+    public synchronized void onReceiveString(TCPConnection tcpConnection, String value) {
+        sendToAllConnections(value);
+    }
+
+    @Override
+    public synchronized void onDisconnect(TCPConnection tcpConnection) {
+        connections.remove(tcpConnection);
+        sendToAllConnections("Client died: " + tcpConnection);
+    }
+
+    @Override
+    public synchronized void onException(TCPConnection tcpConnection, Exception e) {
+        System.out.println("TCPConnection exception: " + e);
+    }
+
+    public void sendToAllConnections(String value) {
+        System.out.println(value);
+        final int cnt = connections.size();
+        for (int i = 0; i < cnt; i++) connections.get(i).sendString(value);
+    }
+}
